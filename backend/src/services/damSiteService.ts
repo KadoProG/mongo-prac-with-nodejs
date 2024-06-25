ca495fda40a9b0adae7897ad89c4061e46448c2c
KadoProG <splatrain@gmail.com>
2024-06-25 15:09:20 +0900

    add 別途scoringAiIdsで指定したデータを取得できるよう変更

--- backend/src/services/damSiteService.ts
+++ backend/src/services/damSiteService.ts
@@ -8,21 +8,21 @@ const cdmCardNo = process.env.CDM_CARD_NO;
 const url = 'https://www.clubdam.com/app/damtomo/scoring/GetScoringAiListXML.do';
 
 export const fetchDamSite = async (
-  pageNo: number
+  options: { pageNo?: number; scoringAiId?: number }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): Promise<{ list: any[]; meta: IMeta }> => {
+  const params = {
+    cdmCardNo,
+    pageNo: options.pageNo ?? undefined,
+    scoringAiId: options.scoringAiId ?? undefined,
+    detailFlg: 1,
+  };
+
   try {
-    const response = await axios.get(url, {
-      params: {
-        cdmCardNo,
-        pageNo,
-        detailFlg: 1,
-      },
-    });
+    const response = await axios.get(url, { params });
 
     const data = response.data;
     const resultJson = await convertXmlToJson(data);
-
     const list = convertDamAiSummary(resultJson);
 
     const meta = convertMeta(resultJson);
@@ -41,27 +41,37 @@ export const fetchDamSiteList = async (options: {
   const resultList = [];
   let pageNo = options.minPage || 1;
   const maxPageNo = options.maxPage || 40;
+  let remainingScoringAiIds: number[] = options.scoringAiIds || [];
 
-  const currentMaxDamScoringAiId = 1;
+  const currentMaxDamScoringAiId = undefined;
 
-  let hasNext = true;
-
-  while (hasNext) {
+  // まずはページ数を指定して取得
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
     // eslint-disable-next-line no-await-in-loop
-    const { list } = await fetchDamSite(pageNo);
+    const { list } = await fetchDamSite({ pageNo });
+
+    if (list.length === 0) break;
 
     resultList.push(...list);
 
-    if (
-      (currentMaxDamScoringAiId &&
-        list.map((data) => data.scoringAiId).includes(currentMaxDamScoringAiId)) ||
-      pageNo === maxPageNo
-    ) {
-      hasNext = false;
-    }
+    if (pageNo === maxPageNo) break;
+
+    const scoringAiIds = list.map((data) => data.scoringAiId);
+    remainingScoringAiIds = remainingScoringAiIds.filter((id) => scoringAiIds.includes(id));
+
+    if (currentMaxDamScoringAiId && scoringAiIds.includes(currentMaxDamScoringAiId)) break;
 
     pageNo += 1;
   }
+
+  // 残りのAiデータを取得
+  await Promise.all(
+    remainingScoringAiIds.map(async (scoringAiId) => {
+      const { list } = await fetchDamSite({ scoringAiId });
+      resultList.push(...list);
+    })
+  );
 
   return resultList;
 };
