4a94ccea816a07f7012e3712d0875eeb867b691f
KadoProG <splatrain@gmail.com>
2024-07-07 14:11:31 +0900

    add 必要なファイルのテンプレの生成

--- backend-3/src/app.module.ts
+++ backend-3/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ScoresModule } from './scores/scores.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), ScoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
