725b4e46e8d50643405402ec3b36356115296658
KadoProG <splatrain@gmail.com>
2024-07-07 11:49:28 +0900

    add express container

---
+++ backend-2/README.md
@@ -0,0 +1,5 @@
+# mongoose を試すための express 環境
+
+ChatGPT に最適なバックエンドフレームワークを伺ったところ、mongoose という自分の知らないフレームワークがあるよとのことでそれを試してみる。
+
+- [公式ドキュメント](https://mongoosejs.com/)
---
+++ backend-2/src/main.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+// 環境変数の読み込み
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
