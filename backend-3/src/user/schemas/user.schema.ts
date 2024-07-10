4783d51e431f2bb11e283bd3ecc9ab2ab1f9ad1f
KadoProG <splatrain@gmail.com>
2024-07-11 07:14:56 +0900

    update passwordをユーザ取得時に非表示に

--- backend-3/src/user/schemas/user.schema.ts
+++ backend-3/src/user/schemas/user.schema.ts
@@ -1,7 +1,7 @@
 import { Schema } from 'mongoose';
 
 export const UserSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
 });
